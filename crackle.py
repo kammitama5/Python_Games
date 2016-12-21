25af5aea627fd48eac712f779fd14341d577e786
kammitama5 <krystal.maughan@gmail.com>
2016-12-20 16:42:47 -0800

    a programme -> crackle

---
+++ crackle.py
@@ -0,0 +1,16 @@
+def Crackle_orCracklePop():
+  for num in range(1, 101):
+    if ((num % 3 == 0) and (num % 5 == 0)):
+      print "CracklePop"
+    elif (num % 3) == 0:
+      print "Crackle"
+    elif (num % 5) == 0:
+      print "Pop"
+    else:
+      print num
+    
+    
+  return
+
+
+Crackle_orCracklePop() 
